f7a114abed988937ae9f69903feb7e645f073148
kokwana-phaks <phaksdevops@gmail.com>
2025-08-12 01:17:51 +0200

    Add: code to Home.jsx for functionality

--- movieapp/frontEnd/src/App.jsx
+++ movieapp/frontEnd/src/App.jsx
@@ -1,18 +1,21 @@
 import "./App.css";
 import MovieCard from "./components/movieCard";
+import Home from "./pages/Home"
 
 function App() {
 
-  const movieNumber = 2;
+  const movieNumber = 1;
   
   return (
     <>
       {movieNumber === 1 ? (
         <MovieCard movie={{title: "Phaks's Film", release_date: "2025"}} />
       ) : (
-        <MovieCard movie={{title: "jacks's Film", release_date: "2025"}} />
+        <MovieCard movie={{title: "Phaks's Film", release_date: "2025"}} />
 
       )}
+
+      <Home />
     </>
   );
 }
