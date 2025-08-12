94813eef2525c114b5269dcc0d92db4e0e669ff6
kokwana-phaks <phaksdevops@gmail.com>
2025-08-12 19:12:53 +0200

    Add: NavBar.jsx and routing

--- movieapp/frontEnd/src/App.jsx
+++ movieapp/frontEnd/src/App.jsx
@@ -1,17 +1,21 @@
 import "./App.css";
 import MovieCard from "./components/movieCard";
 import Favorites from "./pages/Favorites";
-import Home from "./pages/Home"
+import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
+    <div>
+      <NavBar />
     <main className="main-content">
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/favorites" element={<Favorites />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
       </Routes>
     </main>
+    </div>
   );
 }
 
