967aca82b3096e782cefa8356ccff456eedf0b94
kokwana-phaks <phaksdevops@gmail.com>
2025-09-01 00:12:36 +0200

    Create a Food item Component

--- foodapp/src/components/Search.jsx
+++ foodapp/src/components/Search.jsx
@@ -4,7 +4,7 @@ const URL = "https://api.spoonacular.com/recipes/complexSearch";
 const API_KEY = "2ebb3a7dffea425e94ed06a5a193c2fc";
 
 export default function Search({foodData, setFoodData}) {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState("pizza");
 // syntax for the useEffect hook
   useEffect(() => {
     async function fetchFood() {
