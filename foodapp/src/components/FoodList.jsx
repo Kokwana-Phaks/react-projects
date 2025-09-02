cb4e8090843c0d047d0228a6aed969bbec79f19f
kokwana-phaks <phaksdevops@gmail.com>
2025-09-02 15:32:50 +0200

    Add Container.jsx and nest FoodList.jsx

--- foodapp/src/components/FoodList.jsx
+++ foodapp/src/components/FoodList.jsx
@@ -4,8 +4,9 @@ export default function FoodList({ foodData }) {
   return (
     <div>
       {foodData.map((food) => (
-        <FoodItem key={food.id} food={food} />
+       <FoodItem key={food} food={food} />
       ))}
     </div>
   );
 }
+ 
